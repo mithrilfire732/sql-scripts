5fb3f2ef55618d3bdc3ae2b579c75e179c1b14d6
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-09-17 11:17:04 -0400

    changed Grade B to be >= 1000

--- grade-by-sat.sql
+++ grade-by-sat.sql
@@ -2,8 +2,8 @@ select lastname, 'A' as 'ABC' from Student
 	where SAT > 1200
 union
 select lastname, 'B' as 'ABC' from Student
-	where SAT <= 1200 and SAT > 1000
+	where SAT <= 1200 and SAT >= 1000
 union
 select lastname, 'C' as 'ABC' from Student
-	where SAT <= 1000
+	where SAT < 1000
 order by ABC;
